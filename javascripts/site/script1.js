f37cb95464f7c2bebeb264fcbcc6d8f36d73a32d
Colin Broderick <rusty@therustyproject.com>
2013-11-18 21:10:09 +0000

    added entraces to list

--- javascripts/site/script1.js
+++ javascripts/site/script1.js
@@ -1,4 +1,4 @@
-var map = L.mapbox.map('map').setView([38.73300, -9.16], 17);
+var map = L.mapbox.map('map').setView([38.7333, -9.1607], 20);
 
 // Add layer switcher control to the right of the map to change baselayers.
 L.control.layers({
@@ -9,23 +9,28 @@ L.control.layers({
 
 var markerLayer1 = L.mapbox.markerLayer().loadURL('../geojson/facilities.geojson');
 var markerLayer2 = L.mapbox.markerLayer().loadURL('../geojson/transport.geojson');
+var markerLayer3 = L.mapbox.markerLayer().loadURL('../geojson/entrances.geojson');
+
 // reference the map-ui area for the layer toggle.
 var ui = document.getElementById('map-ui');
 
 
 // add layers for the switcher
-addLayer(L.mapbox.tileLayer('rusty.isegi_map'), L.mapbox.gridLayer('rusty.isegi_map'),'UNL Buildings', 4);
+addLayer(1, L.mapbox.tileLayer('rusty.isegi_map'), L.mapbox.gridLayer('rusty.isegi_map'),'UNL Buildings', 4);
 //addLayer(L.mapbox.tileLayer('rusty.isegi_facilities_labels'), L.mapbox.gridLayer('rusty.isegi_facilities_labels'),'UNL Facilities', 5);
-addLayer(markerLayer2, markerLayer2,'Transport Options', 6);
-addLayer(markerLayer1, markerLayer1, 'UNL Facilities', 7);
+addLayer(0, markerLayer3, markerLayer3, 'UNL Building Entrances', 4);
+addLayer(0, markerLayer2, markerLayer2,'Transport Options', 6);
+addLayer(1, markerLayer1, markerLayer1, 'UNL Facilities', 7);
 
 
-function addLayer(layer, gridlayer, name, zIndex) {
-    layer
+function addLayer(active, layer, gridlayer, name, zIndex) {
+    if (active === 1) {
+	layer
         .setZIndex(zIndex)
         .addTo(map);
     gridlayer
         .addTo(map);
+	}
     // add the gridControl the active gridlayer
     var gridControl = L.mapbox.gridControl(gridlayer, {follow: true}).addTo(map);
     // Create a simple layer switcher that toggles layers on and off.
@@ -33,7 +38,9 @@ function addLayer(layer, gridlayer, name, zIndex) {
     var link = document.createElement('a');
 
     link.href = '#';
+	if (active === 1) {
     link.className = 'active';
+} else {link.className = '';}
     link.innerHTML = name;
 
     link.onclick = function(e) {
@@ -60,16 +67,23 @@ markerLayer1.on('layeradd', function(e) {
         feature = marker.feature;
 
     // Create custom popup content
-    var popupContent =  '<h1 style="line-height:0;margin-bottom:0;">' + feature.properties.name + '</h1>' +
-                        '<p>'+ feature.properties.OWNER +'</p>';
+    var popupContent =  '<h1 style="line-height:1;margin-bottom:0;">' + feature.properties.name + '</h1>' +
+                        '<p style="margin-top:0;">'+ feature.properties.OWNER +'</p>';
 
     // http://leafletjs.com/reference.html#popup
     marker.bindPopup(popupContent,{
         closeButton: true,
         minWidth: 100
     });
+	
+	// create array of markers
+	var makers = [];
+	
+	this.eachLayer(function(feature) { makers.push(popupContent); });
+
+	
 	// display a list of markers.
-//	document.getElementById('onscreen').innerHTML = popupContent;
+	document.getElementById('onscreen').innerHTML = makers;
 });
 
 // Add custom popups to each using our custom feature properties
@@ -87,6 +101,7 @@ markerLayer2.on('layeradd', function(e) {
         closeButton: true,
         minWidth: 320
     });
+
 });
 
 $('#search').keyup(search);
@@ -105,3 +120,22 @@ function search() {
             .indexOf(searchString) !== -1;
     }
 }
+
+// Add custom popups to each using our custom feature properties
+markerLayer3.on('layeradd', function(e) {
+    var marker = e.layer,
+        feature = marker.feature;
+
+    // Create custom popup content
+    var popupContent =  '<h1 class="title">To ' + feature.properties.Entrances +  '</h1>';
+
+    // http://leafletjs.com/reference.html#popup
+    marker.bindPopup(popupContent,{
+        closeButton: true,
+        minWidth: 120
+    });
+
+});
+
+
+
